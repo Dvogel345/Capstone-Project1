4469420c93094ef20bd1eefe8ef06073486693fa
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2021-11-30 18:47:38 -0500

    Renamed player-construct.js to index.js

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ let gameloop;
 let player;
 let borders = [];
 
-
 // Create Input Variables
 let rightKey;
 let leftKey;
+let upKey;
+let downKey;
 
 // Runs once page loads
 window.onload = function(){
@@ -24,27 +25,24 @@ window.onload = function(){
 
     // Create Player
     // (x / y coords)
-    player = new Player(460, 420);
+    player = new Player(450, 20)
 
     // Create Borders
-   
-    borders.push(new Border(30, 470, 800, 30, 1))
-    //walls
-   
+    // ground
+    borders.push(new Border(30, 470, 800, 30, 1));
+    // walls 
     borders.push(new Border(0, 0, 30, 800, 2));
-  
-    borders.push(new Border(770, 0,  30, 500, 2))
-    // middle level
+    borders.push(new Border(770, 0,  30, 500, 2));
+    // blocks
+    borders.push(new Border(100, 390, 80, 80, 1));
+    borders.push(new Border(250, 290, 80, 80, 1));
+    borders.push(new Border(400, 290, 80, 80, 1));
+    borders.push(new Border(550, 190, 80, 80, 1));
+    // top platform
+    borders.push(new Border(100, 150, 350, 30, 1));
     
-    borders.push(new Border(100, 320, 600, 30, 1));
-    //top level
-
-    borders.push(new Border(100, 150, 600, 30, 1));
     // Start game loop
     gameLoop = setInterval(step, 1000/30);
-
-    // Create Portals
-    
 
 }
 
@@ -58,37 +56,51 @@ function step() {
 
 function draw() {
     // Clear canvas
-    ctx.fillStyle = "#e6e5e5";
+    ctx.fillStyle = "white";
     ctx.fillRect(0, 0, 800, 500);
 
     // Draw the player
     player.draw();
 
     //Draw the borders
+    // Using a for loop allows to go through the array of dimensions
     for (let i = 0; i < borders.length; i++){
         borders[i].draw();
     }
+    // borders.draw();
 
 }
 
 function setupInputs() {
     document.addEventListener("keydown", function(event) {
-        if (event.key === "ArrowRight") {
+        if (event.key === "ArrowRight" || event.key === "d") {
             // console.log("right");
             rightKey = true;
-        } else if (event.key === "ArrowLeft") {
+        } else if (event.key === "ArrowLeft" || event.key === "a") {
             // console.log("left");
             leftKey = true;
+        } else if (event.key === "ArrowDown" || event.key === "s") {
+            // console.log("down");
+            downKey = true;
+        } else if (event.key === "ArrowUp" || event.key === "w") {
+            // console.log("up");
+            upKey = true;
         }
     });
 
     document.addEventListener("keyup", function(event) {
-        if (event.key === "ArrowRight") {
-            // console.log("right2");
+        if (event.key === "ArrowRight" || event.key === "d") {
+            // console.log("right");
             rightKey = false;
-        } else if (event.key === "ArrowLeft") {
-            // console.log("left2");
+        } else if (event.key === "ArrowLeft" || event.key === "a") {
+            // console.log("left");
             leftKey = false;
+        } else if (event.key === "ArrowDown" || event.key === "s") {
+            console.log("down");
+            downKey = false;
+        } else if (event.key === "ArrowUp" || event.key === "w") {
+            console.log("up");
+            upKey = false;
         }
     });
 }
