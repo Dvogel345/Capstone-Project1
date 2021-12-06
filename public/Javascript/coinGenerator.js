4de67d9bf0f8f4c4c3df3fca9c9b52a11227d891
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2021-12-06 18:02:03 -0500

    not finished

--- public/Javascript/coinGenerator.js
+++ public/Javascript/coinGenerator.js
@@ -1,3 +1,5 @@
+// making coins which cross over the board from the right side
+
 let timeToNextCoin = 0;
 let coinIntervals = 750;
 let lastTime = 0;
@@ -7,7 +9,7 @@ class Coin {
     constructor(){
         this.width = 30;
         this.height = 30;
-        this.x = Math.random() * (canvas.width - this.height);
+        this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 2 + 3;
         this.directionY = Math.random() * 2 - 2.5;
